0822e9b8e7b3a86c773e51953ad4b4c0222a30e2
EstebanCastillo <ncastillo308@unab.edu.co>
2024-02-07 14:33:29 -0500

    Proyecto completado de GuitarLA

---
+++ comprascarrito/src/components/Guitarra.vue
@@ -0,0 +1,42 @@
+<script setup>
+
+
+  const props = defineProps({
+    guitarraprop: {
+      type: Object,
+      required: true
+    }
+  })
+
+  defineEmits(['agregar-carrito'])
+
+  // const incrementar = () => {
+  //   numero.value++
+  // }
+</script>
+
+
+<template>
+  <div class="col-md-6 col-lg-4 my-4 row align-items-center">
+    <div class="col-4">
+      <img class="img-fluid" v-bind:src="'/img/' + guitarraprop.imagen + '.jpg'"
+        :alt="'imagen guitarra-' + guitarraprop.nombre">
+    </div>
+    <div class="col-8">
+      <h3 class="text-black fs-4 fw-bold text-uppercase">{{ guitarraprop.nombre }}</h3>
+      <p>{{ guitarraprop.descripcion }}</p>
+      <p class="fw-black text-primary fs-3">$ {{ guitarraprop.precio }}</p>
+      <button 
+          type="button" 
+          class="btn btn-dark w-100 " 
+          @click="$emit('agregar-carrito', guitarraprop)"
+          
+          >Agregar al Carrito</button>
+    </div>
+  </div>
+  <!-- FIN GUITARRA -->
+</template>
+
+
+
+<style></style>
